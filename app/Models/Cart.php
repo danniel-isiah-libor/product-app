eb34b051c8abd7e0c383247e54ee59bb4913bd2b
Danniel Libor <dlibor.dev@gmail.com>
2025-05-22 16:46:50 +0800

    user->cart relation

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -9,4 +9,9 @@ class Cart extends Model
 {
     /** @use HasFactory<\Database\Factories\CartFactory> */
     use HasFactory;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
